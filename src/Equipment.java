84bfc13fa9083d71691e729cc015f5275681b907
r1chwo0n <110924108+r1chwo0n@users.noreply.github.com>
2023-11-28 12:59:41 +0700

    Add defense

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,6 +1,7 @@
 public class Equipment {
     private static int equipLevel = 1;
     private double damage = 1;
+    private double defense = 1;
     private String equip;
     public Equipment(String equip){
         this.equip = equip;
@@ -8,10 +9,10 @@ public class Equipment {
     public void chooseEquip(){ //instant method เมื่อเลือกอาวุธแล้ว damage ของอาวุธจะแตกต่างกันไป
         if(equip.equalsIgnoreCase("sword")){
             damage*=(1+0.1*equipLevel);
+            System.out.println("equip damage = " + damage);
         }else if(equip.equalsIgnoreCase("shield")){
-            damage*=(1+0.05*equipLevel);
+            System.out.println("equip defense = " + defense);
         }
-        System.out.println("equip damage = " + damage);
     }
     public static void levelUp(){ //class method level ของอาวุธเพิ่มขึ้น
         equipLevel++;
